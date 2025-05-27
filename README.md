9ea154ea0bcb6bf8485b1d92fb71f333c603f84e
nexovec <nexovec@peoplesoftware.org>
2025-05-27 11:07:52 +0200

    readme, setup script and small changes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Helix configuration
+
+> This is made for linux, go away, wicked windows user!
+
+This is my helix configuration, clone this at a location `~/.config/helix`.
+
+Contains a setup script that installs npm(through nvm), python(through uv), go with utility packages, general linux packages, and rust(through rustup). It uses snap and assumes your distro has apt, but if you have snap on another distro, you can skip the apt calls
+
+> TODO: lsp dependencies for all the installed languages
--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ sudo snap install go --classic
 sudo snap install rustup --classic
 rustup toolchain install nightly-x86_64-unknown-linux-gnu
 sudo snap install astral-uv --classic
+# sudo snap install docker # ?? --classic
 sudo snap install pyright --classic
 sudo snap install postman # not strictly needed
 sudo snap install ruff
@@ -20,4 +21,20 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 nvm install node
 nvm use node
 
+# installs tools
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/air-verse/air@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install github.com/google/pprof@latest
+go install github.com/jesseduffield/lazydocker@latest
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/hhatto/gocloc/cmd/gocloc@latest
+go install github.com/dundee/gdu/v5/cmd/gdu@latest
+
+
 # installs lsp servers
+go install golang.org/x/tools/gopls@latest                               # LSP
+go install github.com/go-delve/delve/cmd/dlv@latest                      # Debugger
+go install golang.org/x/tools/cmd/goimports@latest                       # Formatter
+go install github.com/nametake/golangci-lint-langserver@latest           # Linter
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest # Linter cli
