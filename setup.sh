98f0b4ff09aebfaf07b6e9b306605e2859ce2098
marek.sajner <marek.sajner@portabo.cz>
2025-06-06 00:30:46 +0200

    fix: wrong golangci-lint packages

--- setup.sh
+++ setup.sh
@@ -69,5 +69,6 @@ go install github.com/dundee/gdu/v5/cmd/gdu@latest
 go install golang.org/x/tools/gopls@latest                               # LSP
 go install github.com/go-delve/delve/cmd/dlv@latest                      # Debugger
 go install golang.org/x/tools/cmd/goimports@latest                       # Formatter
-go install github.com/nametake/golangci-lint-langserver@latest           # Linter
-go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest # Linter cli
+# go install github.com/nametake/golangci-lint-langserver@latest           # Linter
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+# go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest # Linter cli
